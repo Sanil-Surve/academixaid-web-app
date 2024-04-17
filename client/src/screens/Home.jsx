7b8c5eea1c6955eebf0b34cb18f9118b93200004
Sanil-Surve <sanilsurve42@gmail.com>
2024-04-17 11:52:24 +0530

    initial commit

--- client/src/screens/Home.jsx
+++ client/src/screens/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
 
   const fetchAnswer = async (question) => {
     try {
-      const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/questions`, {
+      const response = await fetch(`%process.env.REACT_APP_BACKEND_URL/api/questions`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
