28c8dbc6f2f595c35d91ac34a4304d883baefd7a
Sanil-Surve <sanilsurve42@gmail.com>
2024-08-10 21:53:08 +0530

    initial commit

--- client/src/components/SearchList.jsx
+++ client/src/components/SearchList.jsx
@@ -1,59 +1,3 @@
-// import React, { useState } from "react";
-// import axios from "axios";
-// import "./SearchList.css";
-// import { NavLink } from "react-router-dom";
-
-// const SearchList = () => {
-//   const [question, setQuestion] = useState("");
-//   const [answer, setAnswer] = useState("");
-
-//   const handleChange = (event) => {
-//     setQuestion(event.target.value);
-//   };
-
-//   const handleSubmit = async (event) => {
-//     event.preventDefault();
-//     try {
-//       const response = await axios.post(
-//         "https://academixaid-app-backend-one-production.up.railway.app/api/fetch-answer",
-//         { question }
-//       );
-//       setAnswer(response.data.answer);
-//     } catch (error) {
-//       console.error("Error fetching answer:", error.message);
-//     }
-//   };
-
-//   return (
-//     <>
-//       <NavLink to="/home" className="home_route">Go to Home</NavLink>
-//       <h2>Search a Query!</h2><br />
-//       <div className="search-list-container">
-//         <form onSubmit={handleSubmit}>
-//           <input
-//             type="text"
-//             value={question}
-//             onChange={handleChange}
-//             placeholder="Search your question..."
-//             className="input-container"
-//           />
-//           <button className="button-container" type="submit">
-//             Search
-//           </button>
-//         </form>
-//         {answer && (
-//           <div className="answer-container">
-//             <h3>Answer:</h3>
-//             <p>{answer}</p>
-//           </div>
-//         )}
-//       </div>
-//     </>
-//   );
-// };
-
-// export default SearchList;
-
 import React, { useState } from "react";
 import axios from "axios";
 import "./SearchList.css";
@@ -70,7 +14,7 @@ const SearchList = () => {
     // Fetch suggestions based on input value
     try {
       const response = await axios.post(
-        "https://academixaid-app-backend-one-production.up.railway.app/api/fetch-suggestions",
+        "https://academixaid-app-backend-one.onrender.com/api/fetch-suggestions",
         { question: value }
       );
       setSuggestions(response.data.suggestions);
@@ -83,7 +27,7 @@ const SearchList = () => {
     event.preventDefault();
     try {
       const response = await axios.post(
-        "https://academixaid-app-backend-one-production.up.railway.app/api/fetch-answer",
+        "https://academixaid-app-backend-one.onrender.com/api/fetch-answer",
         { question }
       );
       setAnswer(response.data.answer);
