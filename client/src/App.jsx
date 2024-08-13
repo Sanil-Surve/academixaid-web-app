92733d16f38d9882a29485fc35f3037abd1004a5
Sanil-Surve <sanilsurve42@gmail.com>
2024-08-13 20:17:56 +0530

    initial commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,52 +1,25 @@
-// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import SignUp from "./screens/SignUp";
-// import Login from "./screens/Login";
-// import Home from "./screens/Home";
-// import Landing from "./screens/Landing";
-// import View from "./screens/View";
-// import Chat from "./screens/Chat";
-
-// const App = () => {
-//   return (
-//     <Router>
-//       <Routes>
-//         <Route path="/signup" element={<SignUp />} exact/>
-//         <Route path="/login" element={<Login />} exact/>
-//         <Route path="/home" element={<Home />} exact/>
-//         <Route path="/" element={<Landing />} exact/>
-//         <Route path="/search" element={<View />} exact/>
-//         <Route path="/chat" element={<Chat />} exact/>
-//       </Routes>
-//     </Router>
-//   );
-// };
-
-// export default App;
-
-import React, { lazy } from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// Lazy load the components
-const SignUp = lazy(() => import('./screens/SignUp'));
-const Login = lazy(() => import('./screens/Login'));
-const Home = lazy(() => import('./screens/Home'));
-const Landing = lazy(() => import('./screens/Landing'));
-const View = lazy(() => import('./screens/View'));
-const Chat = lazy(() => import('./screens/Chat'));
+import SignUp from "./screens/SignUp";
+import Login from "./screens/Login";
+import Home from "./screens/Home";
+import Landing from "./screens/Landing";
+import View from "./screens/View";
+import Chat from "./screens/Chat";
 
 const App = () => {
   return (
     <Router>
-        <Routes>
-          <Route path="/signup" element={<SignUp />} exact />
-          <Route path="/login" element={<Login />} exact />
-          <Route path="/home" element={<Home />} exact />
-          <Route path="/" element={<Landing />} exact />
-          <Route path="/search" element={<View />} exact />
-          <Route path="/chat" element={<Chat />} exact />
-        </Routes>
+      <Routes>
+        <Route path="/signup" element={<SignUp />} exact/>
+        <Route path="/login" element={<Login />} exact/>
+        <Route path="/home" element={<Home />} exact/>
+        <Route path="/" element={<Landing />} exact/>
+        <Route path="/search" element={<View />} exact/>
+        <Route path="/chat" element={<Chat />} exact/>
+      </Routes>
     </Router>
   );
 };
 
 export default App;
+
